60b3c2f421925dfe044a0b289dbb675e3f17e666
jay459 <anigounigoud@gmail.com>
2021-02-10 12:02:08 +0530

    auth routes

---
+++ middleware/auth.js
@@ -0,0 +1,26 @@
+const jwt = require('jsonwebtoken')
+const User = require('../models/usermodel');
+const ErrorHandler = require('../utils/errorhandler');
+
+const isAuthenticatedUser =  async (req, res, next) => {
+    const { token } = req.cookies
+    //console.log(token);
+    if(!token) {
+        return next(new ErrorHandler('Please Login',401))
+    }
+
+    const decoded = jwt.verify(token , process.env.JWT_SECRET)
+    req.user = await User.findById(decoded._id)
+    next();
+}
+
+const authRoles = (...roles)=>{
+    return (req, res, next)=>{
+        if(!roles.includes(req.user.role)){
+            return next(new ErrorHandler(`Role ${req.user.role} doest not have access to this resource`,403));
+        }
+        next();
+    }
+}
+
+module.exports = {isAuthenticatedUser, authRoles}
